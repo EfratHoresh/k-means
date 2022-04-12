e7ce1758613f9e37a84693c4973306ce66f629b5
Efrat <efratporten@mail.tau.ac.il>
2022-04-12 10:34:15 +0300

    python finished - no testing

--- main.py
+++ main.py
@@ -1,62 +1,83 @@
-def kmeans(k, max_iter=200, input, output):
-    inputFile = open(input) #get vectors from file
-    lines = inputFile.readlines()
-    inputFile.close()
+def kmeans(k, input, max_iter=200):
+    input_file = open(input)  # get vectors from file
+    lines = input_file.readlines()
+    input_file.close()
     vectors = []
     for line in lines:
-        vectors.append(line.split(","))
-    n = len(vectors) #update n and d
+        tmp_vector = line.split(",")
+        vectors.append([float(i) for i in tmp_vector])
+    n = len(vectors)  # update n and d
     d = len(vectors[0])
-    e=0.001
-    iterationNumber = 0
-    if k<=1 or k>=n or max_iter<1: #check input
+    e = 0.001
+    iteration_number = 0
+    if k <= 1 or k >= n or max_iter < 1:  # check input
         print("Invalid Input!")
         return
     centroids = [vectors[i] for i in range(k)]
     while True:
         clusters = [[] for i in range(k)]
-        for x in vectors: #assign vectors to clusters
+        for x in vectors:  # assign vectors to clusters
             j = argmin(x, centroids)
             clusters[j].append(x)
         norms = [0 for i in range(k)]
-        for i in range(k): #update centroids
-            newCentroid = updateU(clusters[i])
-            norms[i] = (oclidDistance(newCentroid, centroids[i]))**0.5
-            centroids[i] = newCentroid
-        iterationNumber+=1
-        if max(norms)<e or iterationNumber == max_iter: #check convergence
+        for i in range(k):  # update centroids
+            new_centroid = updateU(clusters[i])
+            norms[i] = (oclidDistance(new_centroid, centroids[i]))**0.5
+            centroids[i] = new_centroid
+        iteration_number += 1
+        if max(norms) < e or iteration_number == max_iter:  # check convergence
             break
-    outputFile = open(output, "w")
-    for centroid in centroids:
-        outputFile.write(','.join(centroid)+"/n")
-    outputFile.close
-    return
+    return centroids
 
 
 def oclidDistance(x1, x2):
     distance = 0
     for i in range(len(x1)):
-        distance+= (x1[i]-x2[i])**2
+        distance += (x1[i]-x2[i])**2
     return distance
 
 
 def argmin(x, centroids):
     index = 0
-    minDistance = oclidDistance(x, centroids[0])
+    min_distance = oclidDistance(x, centroids[0])
     for j in range(1, len(centroids)):
         distance = oclidDistance(x, centroids[j])
-        if distance < minDistance:
-            minDistance = distance
+        if distance < min_distance:
+            min_distance = distance
             index = j
     return index
 
 
 def updateU(cluster):
     d = len(cluster[0])
-    newU = [0 for i in range(d)]
+    new_u = [0 for i in range(d)]
     for vector in cluster:
         for i in range(d):
-            newU[i]+=vector[i]
-    for i in newU:
-        newU[i] = newU[i]/(len(cluster))
-    return newU
+            new_u[i] += vector[i]
+    for i in range(len(new_u)):
+        new_u[i] = new_u[i]/(len(cluster))
+    return new_u
+
+args = input().split()
+if len(args) == 4:
+    if not args[0].isnumeric() or not args[1].isnumeric():
+        print("Invalid Input!")
+        quit()
+    centroids = kmeans(int(args[0]), args[2], int(args[1]))
+elif len(args) == 3:
+    if not args[0].isnumeric():
+        print("Invalid Input!")
+        quit()
+    centroids = kmeans(int(args[0]), args[1])
+else:
+    print("Invalid Input!")
+    quit()
+output_file = open(args[3], "w")
+for centroid in centroids:
+    centroid_string = ['{:.4f}'.format(x) for x in centroid]
+    output_file.write(','.join(centroid_string)+"\n")
+output_file.close
+
+
+# if __name__ == "main":
+#     main()
