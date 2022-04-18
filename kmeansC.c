0469ef10ad748ac695bdc14cd22bfe6c2cdbdae8
Inbal <inbalpearl@mail.tau.ac.il>
2022-04-18 19:49:01 +0300

    Try to fix bugs in nova

--- kmeansC.c
+++ kmeansC.c
@@ -15,19 +15,19 @@ void write_in_file(char output[], double** centroids, int k, int d);
 
 
 int main(int argc, char* argv[]){
-    if (argc!=4 && argc!=5) {  // wrong number of args
+    if (argc!=4 && argc!=5) {  /* wrong number of args */
         printf("Invalid Input!");
         return 0;
     }
-    if (!is_number(argv[1])) {  // k is not a number
+    if (!is_number(argv[1])) {  /* k is not a number */
         printf("Invalid Input!");
         return 0;
     }
-    if (argc==4) {  //no max_iter given
+    if (argc==4) {  /* no max_iter given */
         kmeans(atoi(argv[1]), 200, argv[2], argv[3]);
     }
-    else {  //(argc==5) max_iter given
-        if (!is_number(argv[2]) || atoi(argv[2]) <= 0) {  // max_iter not a number or not positive
+    else {  /* (argc==5) max_iter given */
+        if (!is_number(argv[2]) || atoi(argv[2]) <= 0) {  /* max_iter not a number or not positive */
             printf("Invalid Input!");
             return 0;
         }
@@ -37,78 +37,84 @@ int main(int argc, char* argv[]){
 }
 
 void kmeans(int k, int max_iter, char input[], char output[]) {
+    int n = 0;
     int n_d[2] = {0, 0};
     discover_n_d(input, n_d);
-    int n = n_d[0];
+    n = (int)n_d[0];
     int d = n_d[1];
     double e = 0.001;
-    if (k<=1 || k>=n) {  // 0 < k <= n
+    if (k<=1 || k>=n) {  /* 0 < k <= n */
         printf("Invalid Input!");
         exit(1);
     }
-    double** vectors = malloc(n*sizeof(double*));  //create matrix
+    double** vectors = malloc(n*sizeof(double*));  /* create matrix */
     if(vectors == NULL) {
         printf("An Error Has Occurred");
-        // add FREEEE!!!!
+        /* add FREEEE!!!! */
         exit(1);
     }
-    for (int i =0; i <n; i++){
+    int i = 0;
+    for (i =0; i <n; i++){
         vectors[i] = malloc(d*sizeof(double));
-        if(vectors[i] == NULL) { // check if there's enough memory for vectors
+        if(vectors[i] == NULL) { /* check if there's enough memory for vectors */
             printf("An Error Has Occurred");
-            // for(int j = i; j >= 0; j--) {
-            //     free(vectors[j]);
-            // }
-            // free(vectors);
+            /* for(int j = i; j >= 0; j--) {
+                 free(vectors[j]);
+             }
+             free(vectors); */
             exit(1);
         }
     }
     fill_mat(input, vectors);
-    double** centroids = malloc(k*sizeof(double*));  //create centroids
+    double** centroids = malloc(k*sizeof(double*));  /* create centroids */
     if(centroids == NULL) {
         printf("An Error Has Occurred");
-        // add FREEEE!!!!
+        /* add FREEEE!!!! */
         exit(1);
     }
-    for (int i =0; i < k; i++){
+    
+    for (i =0; i < k; i++){
         centroids[i] = malloc(d*sizeof(double));
         if(centroids[i] == NULL) {
             printf("An Error Has Occurred");
-            // add FREEEE!!!!
+            /* add FREEEE!!!! */
             exit(1);
         }
-        for (int j =0; j <d; j++){ // copy vectors values
+        int j = 0;
+        for (j =0; j <d; j++){ /* copy vectors values */
             centroids[i][j] = vectors[i][j];
         }
     }
 
-    int* cluster_of_vector = malloc(n*sizeof(int)); //cluster_of_vector[i]==cluster of xi vector
-    double* norms = malloc(k*sizeof(double));  // oclid distance between new and old centroid
+    int* cluster_of_vector = malloc(n*sizeof(int)); /* cluster_of_vector[i]==cluster of xi vector */
+    double* norms = malloc(k*sizeof(double));  /* oclid distance between new and old centroid */
     double* new_centroid = malloc(d*sizeof(double));
     if(cluster_of_vector == NULL || norms == NULL || new_centroid == NULL) {
         printf("An Error Has Occurred");
-        // add FREEEE!!!!
+        /* add FREEEE!!!! */
         exit(1);
     }
     int num_of_loops = 0;
     while(1 == 1){
-        for (int i=0; i<n; i++) {  // find closest cluster for each vector
+        for (i=0; i<n; i++) {  /* find closest cluster for each vector */
             cluster_of_vector[i] = argmin(vectors[i], centroids, k, d);
         }
-                
-        for (int clust_num=0; clust_num<k; clust_num++) {  // calculate new centroid and norms
-            for (int j=0; j<d; j++) {
+
+        int clust_num = 0;   
+        int j = 0;             
+        for (clust_num=0; clust_num<k; clust_num++) {  /* calculate new centroid and norms */
+            for (j=0; j<d; j++) {
                 new_centroid[j] = 0.0;
             }
             update_u(new_centroid, cluster_of_vector, vectors, d, n, clust_num);
-            norms[clust_num] = sqrt(occlid_distance(new_centroid, centroids[clust_num], d));  // oclid distance between new and old centroid
-            for (int j=0; j<d; j++) {  // update centroid
+            norms[clust_num] = sqrt(occlid_distance(new_centroid, centroids[clust_num], d));  /* oclid distance between new and old centroid */
+            for (j=0; j<d; j++) {  /* update centroid */
                 centroids[clust_num][j] = new_centroid[j];
             }
         }
         num_of_loops++;
         int is_bigger_than_e = 0;
-        for (int i=0; i<k; i++) {  // check if any norm is bigger than e
+        for (i=0; i<k; i++) {  /* check if any norm is bigger than e */
             if (norms[i] >= e) {
                 is_bigger_than_e = 1;
                 break;
@@ -119,7 +125,7 @@ void kmeans(int k, int max_iter, char input[], char output[]) {
         }
     }
 
-    for (int i =0; i <n; i++){  // free vectors matrix
+    for (i =0; i <n; i++){  /* free vectors matrix */
         free(vectors[i]);
     }
     free(vectors);
@@ -127,7 +133,7 @@ void kmeans(int k, int max_iter, char input[], char output[]) {
     free(norms);
     free(new_centroid);
     write_in_file(output, centroids, k, d);
-    for (int i =0; i <k; i++){  // free centroids matrix
+    for (i =0; i <k; i++){  /* free centroids matrix */
         free(centroids[i]);
     }
     free(centroids);
@@ -142,7 +148,7 @@ void discover_n_d(char input[], int n_d[2]) {
         printf("An Error Has Occurred");
         exit(1);
     }
-    char line[1000];  // check length of max line
+    char line[1000];  /* check length of max line */
     int n = 0;
     int d = 0;
     while (fgets(line, 1000, inputFile)) {  //searching for n and d
@@ -162,13 +168,13 @@ void discover_n_d(char input[], int n_d[2]) {
 
 void fill_mat(char input[], double** vectors) {
     FILE* inputFile;
-    char line[1000];  // check max length
+    char line[1000];  /* check max length */
     int i = 0;
     int j = 0;
     inputFile = fopen(input, "r");
     if (inputFile == NULL) {
         printf("An Error Has Occurred");
-        // add FREEEE!!!!
+        /* add FREEEE!!!! */
         exit(1);
     }
     while (fgets(line, 1000, inputFile)) {
@@ -185,8 +191,9 @@ void fill_mat(char input[], double** vectors) {
     return;
 }
 
-int is_number(char s[]) {  // check if string is int
-    for (int i = 0; i < strlen(s); i++)
+int is_number(char s[]) {  /* check if string is int */
+    int i = 0;
+    for (i = 0; i < strlen(s); i++)
         if (isdigit(s[i]) == 0)
             return 0;
     return 1;
@@ -194,17 +201,19 @@ int is_number(char s[]) {  // check if string is int
 
 double occlid_distance(double x1[], double x2[], int d) {
     double distance = 0;
-    for (int i=0; i < d ;i++) {
+    int i = 0;
+    for (i=0; i < d ;i++) {
         distance += pow(x1[i]-x2[i], 2);
     }
     return distance;
 }
 
-int argmin(double x[], double** centroids, int k, int d) {  // returns the index of the cluster
+int argmin(double x[], double** centroids, int k, int d) {  /* returns the index of the cluster */
     int index = 0;
     double min_distance = INFINITY;
     double distance = 0;
-    for (int j=0; j<k; j++) {  // find closest centroid
+    int j = 0;
+    for (j=0; j<k; j++) {  /* find closest centroid */
         distance = occlid_distance(x, centroids[j], d);
         if (distance < min_distance) {
             min_distance = distance;
@@ -216,15 +225,17 @@ int argmin(double x[], double** centroids, int k, int d) {  // returns the index
 
 void update_u(double* new_centroid, int* cluster_of_vector, double** vectors, int d, int n, int clust_num) {
     int num_vectors_in_cluster = 0;
-    for (int i=0; i<n; i++) {  // finds all vectors in cluster
+    int i = 0;
+    int j = 0;
+    for (i=0; i<n; i++) {  /* finds all vectors in cluster */
         if (cluster_of_vector[i]==clust_num) {
             num_vectors_in_cluster++;
-            for (int j=0; j<d; j++) {
+            for (j=0; j<d; j++) {
                 new_centroid[j] +=vectors[i][j];
             }
         }
     }
-    for (int i=0; i<d; i++) {
+    for (i=0; i<d; i++) {
         new_centroid[i] = new_centroid[i] / (double)num_vectors_in_cluster;
     }
     return;
@@ -234,11 +245,13 @@ void write_in_file(char output[], double** centroids, int k, int d) {
     FILE * output_file = fopen(output, "w");
     if (output_file == NULL) {
         printf("An Error Has Occurred");
-        // add FREEEE!!!!
+        /* add FREEEE!!!! */
         exit(1);
     }
-    for (int i = 0; i<k; i++) {
-        for (int j=0; j<d-1; j++) {
+    int i = 0; 
+    int j = 0;   
+    for (i = 0; i<k; i++) {
+        for (j=0; j<d-1; j++) {
             fprintf(output_file, "%.4f,", centroids[i][j]);
         }
         fprintf(output_file, "%.4f\n", centroids[i][d-1]);
